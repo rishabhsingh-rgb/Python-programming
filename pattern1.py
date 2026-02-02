44c800bcde7facd57bd07aca663fff2610e791cd
Rishabh Singh <rishabhsingh742006@gmail.com>
2026-02-02 22:12:46 +0530

    Prints right angled triangle pattern for desired number of rows

---
+++ pattern1.py
@@ -0,0 +1,9 @@
+n=int(input('Enter the number of rows: '))   
+
+for i in range(n,0,-1):
+    for j in range(i,1,-1):
+        print(' ',end=" ")
+    for  k in range(n+1,i,-1):
+        print('*',end=" ") 
+
+    print()
